a72f072cd4e62974517bb16088139718fb8cc47e
DanieleCruz <danisousacruz@yahoo.com.br>
2024-07-22 17:44:52 -0300

    Format strings

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ except:
     print('Tente novamente utilizando valores numericos')
 else:
     if point >= 48 and point <= 53:
-        print(f'Mal Passado')
+        print(f'\nMal Passado')
     elif point >= 54 and point <= 59:
-        print(f'Ao ponto para mal passado')
+        print(f'\nAo ponto para mal passado')
     elif point >= 60 and point <= 64:
-        print(f'Ao ponto')
+        print(f'\nAo ponto')
     elif point >= 65 and point <= 69:
-        print(f'Ao Ponto para bem passado')
+        print(f'\nAo Ponto para bem passado')
     elif point >= 70 and point <= 89:
-        print(f'Bem passado')    
+        print(f'\nBem passado')    
     elif point >= 80:
-         print(f'Passado')
+         print(f'\nPassado')
     elif point <= 47:
-        print(f'A carne está Crua')
+        print(f'\nA carne está Crua')
 
         
